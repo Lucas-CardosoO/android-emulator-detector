444c1d13692d33765593585bcf401fd084cb37bf
Lucas Cardoso <lccao@cin.ufpe.br>
2022-01-17 20:38:06 -0300

    chore(): setup

---
+++ android-emulator-detector/app/src/main/java/com/lccao/androidemulatordetector/CollectedDataModel.kt
@@ -0,0 +1,3 @@
+package com.lccao.androidemulatordetector
+
+data class CollectedDataModel(var collectionDescription: String, var collectedData: String, var collectionTimestamp: Long)
---
+++ android-emulator-detector/app/src/main/java/com/lccao/androidemulatordetector/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lccao.androidemulatordetector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun share(view: android.view.View) {}
+}
