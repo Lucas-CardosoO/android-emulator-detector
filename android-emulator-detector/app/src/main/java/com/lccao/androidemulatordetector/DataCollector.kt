ec7642ff98170e7a95629a181f3060be71865900
Lucas Cardoso <lccao@cin.ufpe.br>
2022-02-16 20:35:33 -0300

    feat(DataCollector.kt): finishes data collection and adds mock functions

--- android-emulator-detector/app/src/main/java/com/lccao/androidemulatordetector/DataCollector.kt
+++ android-emulator-detector/app/src/main/java/com/lccao/androidemulatordetector/DataCollector.kt
@@ -1,16 +1,31 @@
 package com.lccao.androidemulatordetector
 
 import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicReference
 
 object DataCollector {
     val isCollecting: AtomicBoolean = AtomicBoolean(false)
     val hasCollected: AtomicBoolean = AtomicBoolean(false)
-    val dataCollectorsList: List<() -> DataCollector> = listOf()
+    val dataCollectorsList: List<() -> CollectedDataModel> = listOf(this::mockFun1, this::mockFun2)
+    val collectedDataList: AtomicReference<MutableList<CollectedDataModel>> = AtomicReference(mutableListOf())
 
     fun startCollection() {
         isCollecting.set(true)
         dataCollectorsList.forEach {
-
+            val begin = System.currentTimeMillis()
+            var collectedData = it.invoke()
+            val end = System.currentTimeMillis()
+            collectedData.collectionTimestamp = end - begin
+            collectedDataList.get().add(collectedData)
         }
+        hasCollected.set(true)
+    }
+
+    fun mockFun1(): CollectedDataModel {
+        return CollectedDataModel("a", "b", 0)
+    }
+
+    fun mockFun2(): CollectedDataModel {
+        return CollectedDataModel("c", "d", 0)
     }
 }
--- android-emulator-detector/app/src/main/java/com/lccao/androidemulatordetector/MainActivity.kt
+++ android-emulator-detector/app/src/main/java/com/lccao/androidemulatordetector/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DataCollector.startCollection()
     }
 
     fun share(view: android.view.View) {
