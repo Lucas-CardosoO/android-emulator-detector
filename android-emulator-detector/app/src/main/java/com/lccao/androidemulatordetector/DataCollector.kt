794dd540364a655bf02bae517669a27b7d4e57e4
Lucas Cardoso <lucascardoso@Lucass-Mac-mini.local>
2022-06-21 09:48:51 -0300

    feat(DataCollector): adds qemu drivers detection + begin props detection

--- android-emulator-detector/app/src/main/java/com/lccao/androidemulatordetector/DataCollector.kt
+++ android-emulator-detector/app/src/main/java/com/lccao/androidemulatordetector/DataCollector.kt
@@ -1,11 +1,19 @@
 package com.lccao.androidemulatordetector
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Build
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.coroutineScope
 import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.lang.reflect.Method
 import java.util.concurrent.atomic.AtomicReference
+
+typealias PropertyName = String
+typealias PropertyValue = String
+typealias Property = Pair<PropertyName, PropertyValue?>
 
 @DelicateCoroutinesApi
 object DataCollector {
@@ -27,7 +35,7 @@ object DataCollector {
             "fstab.andy",
             "ueventd.andy.rc",
         ),
-        // x86 Files
+        // x86 Files only marked as Emulator when detected along with 5 qemu system properties
         "x86" to listOf(
             "ueventd.android_x86.rc",
             "x86.prop",
@@ -47,8 +55,28 @@ object DataCollector {
 
     private val QEMU_DRIVERS = arrayOf("goldfish")
 
+    private val PROPERTIES = listOf(
+        Property("init.svc.qemud", null),
+        Property("init.svc.qemu-props", null),
+        Property("qemu.hw.mainkeys", null),
+        Property("qemu.sf.fake_camera", null),
+        Property("qemu.sf.lcd_density", null),
+        Property("ro.bootloader", "unknown"),
+        Property("ro.bootmode", "unknown"),
+        Property("ro.hardware", "goldfish"),
+        Property("ro.kernel.android.qemud", null),
+        Property("ro.kernel.qemu.gles", null),
+        Property("ro.kernel.qemu", "1"),
+        Property("ro.product.device", "generic"),
+        Property("ro.product.model", "sdk"),
+        Property("ro.product.name", "sdk"),
+        Property("ro.serialno", null),
+    )
+
+    private val MIN_PROPERTIES_THRESHOLD = 5
+
     private val dataCollectorsList: List<() -> CollectedDataModel> =
-        listOf(this::isEmu, this::buildCharacteristics, this::emulatorFiles)
+        listOf(this::isEmu, this::buildCharacteristics, this::emulatorFiles, this::checkQEmuDrivers)
     val collectedDataList: AtomicReference<MutableList<CollectedDataModel>> =
         AtomicReference(mutableListOf())
 
@@ -114,6 +142,66 @@ object DataCollector {
             }
         }
         return detectedFiles
+    }
+
+    private fun checkQEmuDrivers(): CollectedDataModel {
+        val driversFiles = arrayOf(File("/proc/tty/drivers"), File("/proc/cpuinfo"))
+        for (drivers_file in driversFiles) {
+            if (drivers_file.exists() && drivers_file.canRead()) {
+                val data = ByteArray(1024)
+                try {
+                    val inputStream: InputStream = FileInputStream(drivers_file)
+                    inputStream.read(data)
+                    inputStream.close()
+                } catch (exception: Exception) {
+                    exception.printStackTrace()
+                    return CollectedDataModel("Quemu known drivers", mapOf("Collection Failed with exception" to exception.toString()))
+                }
+                val driver_data = String(data)
+                for (known_qemu_driver in QEMU_DRIVERS) {
+                    if (driver_data.contains(known_qemu_driver)) {
+                        return CollectedDataModel("Quemu known drivers", mapOf(known_qemu_driver to "true"))
+                    }
+                }
+            }
+        }
+        return CollectedDataModel("Quemu known drivers", mapOf("No file was detected, files checked" to driversFiles.toString()))
+    }
+
+//    private fun checkQEmuProps(): Boolean {
+//        var found_props = 0
+//        for (property in PROPERTIES) {
+//            property.
+////            val property_value: String = getProp(AppConte, property.PropertyName)
+//            if (property.seek_value == null && property_value != null) {
+//                found_props++
+//            }
+//            if (property.seek_value != null
+//                && property_value.contains(property.seek_value)
+//            ) {
+//                found_props++
+//            }
+//        }
+//        if (found_props >= MIN_PROPERTIES_THRESHOLD) {
+//            log("Check QEmuProps is detected")
+//            return true
+//        }
+//        return false
+//    }
+
+    @SuppressLint("PrivateApi")
+    private fun getProp(context: Context, property: String): String? {
+        try {
+            val classLoader: ClassLoader = context.getClassLoader()
+            val systemProperties = classLoader.loadClass("android.os.SystemProperties")
+            val get: Method = systemProperties.getMethod("get", String::class.java)
+            val params = arrayOfNulls<Any>(1)
+            params[0] = property
+            return get.invoke(systemProperties, params) as String?
+        } catch (exception: java.lang.Exception) {
+            // empty catch
+        }
+        return null
     }
 }
 
